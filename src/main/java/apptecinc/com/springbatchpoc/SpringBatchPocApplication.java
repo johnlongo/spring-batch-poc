d93b33e0b67ecd183ff43f05f6f0b689f28cc969
Jenkins <john.longo@apptecinc.com>
2023-07-24 18:06:20 -0400

    BATCH DB

--- src/main/java/apptecinc/com/springbatchpoc/SpringBatchPocApplication.java
+++ src/main/java/apptecinc/com/springbatchpoc/SpringBatchPocApplication.java
@@ -2,12 +2,9 @@ package apptecinc.com.springbatchpoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class
-})
+@SpringBootApplication()
 @EnableScheduling
 public class SpringBatchPocApplication {
 
